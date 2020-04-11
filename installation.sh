9b53c34d2d065acc5530d800680491eb32a4fccb
vahid <v.babaee@Gmail.com>
2020-04-11 17:33:17 +0430

    افزودن فایل روش نصب پیش نیازها

---
+++ installation.sh
@@ -0,0 +1,14 @@
+#python3 -m pip install tehran-stocks
+#python -c 'print("---------------")'
+
+#python3 -m pip install sos
+#python -c 'print("---------------")'
+
+python3 -m pip install matplotlib
+#python -c 'print("---------------")'
+
+python3 -m pip install numpy
+#python -c 'print("---------------")'
+
+python3 -m pip install wget
+#python -c 'print("---------------")'
